c4f7b723b27837f20855bd8b5b7bc55f7934308d
Emmanuel Janssens <emmanuel.janssens@heig-vd.ch>
2019-05-21 17:29:52 +0200

    Fonction affichage
    Affichage des dates

--- main.c
+++ main.c
@@ -11,11 +11,30 @@ typedef struct {
     unsigned year;
 } Date;
 
+//display single date
+void printDate(Date date)
+{
+    printf("%02d/%02d/%d\n",date.day,date.month+1,date.year);
+}
+
+//display array of dates
+void printDates(Date* date,int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        printDate(*(date+i));
+    }
+}
+
 int main() {
 
     Date tab[] = {{1,  JAN, 2013},
                   {21, MAY, 2019},
                   {28, FEB, 2002}};
     
+    Date date = {28,JUN,2019};
+    printDate(date);
+    printDates(tab,3);
+
     return 0;
 }
